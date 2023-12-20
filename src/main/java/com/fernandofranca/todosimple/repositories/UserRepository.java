67ee5b300d1c6fde3867e54996391f584fd1ad80
unknown <soufadofernando@gmail.com>
2023-12-20 20:28:01 -0300

    Creating User Repository

---
+++ src/main/java/com/fernandofranca/todosimple/repositories/UserRepository.java
@@ -0,0 +1,13 @@
+package com.fernandofranca.todosimple.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.fernandofranca.todosimple.models.User;
+import java.util.List;
+
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long>{   
+    
+}
